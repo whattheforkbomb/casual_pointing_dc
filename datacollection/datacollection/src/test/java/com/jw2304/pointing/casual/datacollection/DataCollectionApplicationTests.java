c90170623c4082f252350937f009d96f0cff778e
whattheforkbomb <whattheforkbomb+github@gmail.com>
2023-08-06 20:30:08 +0100

    init

---
+++ datacollection/datacollection/src/test/java/com/jw2304/pointing/casual/datacollection/DataCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw2304.pointing.casual.datacollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ tasks/tasks/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.2"
+	id("io.spring.dependency-management") version "1.1.2"
+}
+
+group = "com.jw2304.pointing.casual"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.apache.camel.springboot:camel-spring-boot-starter:4.0.0-RC2")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("com.h2database:h2")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
